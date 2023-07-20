5261ec68d0c67cb33adca5e0165c916d61a18e45
OhHyukJun <kmh7277@naver.com>
2023-07-20 14:55:03 +0900

    Feat: api 수정

--- frontend-react/src/Pages/Auth/Register/api.ts
+++ frontend-react/src/Pages/Auth/Register/api.ts
@@ -5,6 +5,10 @@ const BASE_URL = `http://localhost:${process.env.REACT_APP_SPRING_PORT}`;
 export const memberPost = async (data: any) => {
   const URL = `${BASE_URL}/user/add`;
   console.log("URL:", URL);
-  const res = await axios.post(URL, data);
+  const res = await axios.post(URL,{"body": data},{
+    headers: {
+      'Content-Type':'application/json'
+    },
+  });
   return res.data;
 };
